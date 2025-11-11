ed36674839053295e6ec64385d827e915b770ef9
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-11-11 12:52:53 +0100

    Klar med lastbil

--- src/Lastbil.java
+++ src/Lastbil.java
@@ -1,4 +1,22 @@
-public class Lastbil {
+public class Lastbil extends Vagfordon {
 
-    
+    private double lastkapacitet;
+
+    public Lastbil(String registreringsnummer, String färg, String ägare, double vikt, double lastkapacitet){
+        super(registreringsnummer, färg, ägare, vikt);
+        if (lastkapacitet <= 0){
+            throw new IllegalArgumentException("Lastkapacitet kan inte vara mindre än haha");
+        }
+        this.lastkapacitet = lastkapacitet;
+    }
+
+    public double getLastkapacitet() {
+        return lastkapacitet;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "Lastkapacitet: " + lastkapacitet + "Kg";
+    }
+
 }
